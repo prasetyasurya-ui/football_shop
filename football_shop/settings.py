ba3cded471551d9081b0fa409f3f17f1aedfbbba
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2025-09-16 21:27:41 +0700

    Fixed CSRF_TRUSTED_ORIGIN

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "prasetya-surya-footballshop.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://prasetya-surya-footballshop.pbp.cs.ui.ac.id/"
+    "https://prasetya-surya-footballshop.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
