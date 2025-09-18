6a472daeeb4a3d3cb1fa539482ce5ab549e99a51
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2025-09-18 19:43:52 +0700

    Fixed Typo Bug and Implemented Login, Logout and Register

--- main/views.py
+++ main/views.py
@@ -3,15 +3,30 @@ from django.core import serializers
 from django.http import HttpResponse
 from main.forms import ProductForm
 from main.models import Item
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
 
+@login_required(login_url='/login')
 def show_main(request):
-    product_list = Item.objects.all()
+    filter_type = request.GET.get("filter", "all")
+
+    if filter_type == "all":
+        product_list = Item.objects.all()
+    else:
+        product_list = Item.objects.filter(user=request.user)
 
     context = {
         'shopName': "Wolverhampton Shop",
         'nama': 'Prasetya Surya Syahputra',
         'kelas': 'PBP E',
-        'product_list': product_list
+        'product_list': product_list,
+        'last_login': request.COOKIES.get('last_login', 'Never'),
+        'username': request.user.username
     }
 
     return render(request, "main.html", context)
@@ -20,12 +35,15 @@ def create_product(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product_entry = form.save(commit = False)
+        product_entry.user = request.user
+        product_entry.save()
         return redirect('main:show_main')
     
     context = {'form': form}
     return render(request, "create_product.html", context)
 
+@login_required(login_url='/login')
 def show_product(request, product_id):
     product = get_object_or_404(Item, pk=product_id)
 
@@ -48,7 +66,7 @@ def show_xml_by_id(request, product_id):
         product = Item.objects.filter(pk = product_id)
         xml_data = serializers.serialize("xml", product)
         return HttpResponse(xml_data, content_type="application/xml")
-    except News.DoesNotExist:
+    except Item.DoesNotExist:
         return HttpResponse(status=404)
 
 def show_json_by_id(request, product_id):
@@ -56,6 +74,39 @@ def show_json_by_id(request, product_id):
         product = Item.objects.get(pk = product_id)
         json_data = serializers.serialize("json", [product])
         return HttpResponse(json_data, content_type="application/json")
-    except News.DoesNotExist:
+    except Item.DoesNotExist:
         return HttpResponse(status=404)
 
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form': form}
+    return render(request, 'register.html', context)\
+
+def login_user(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+    else:
+        form = AuthenticationForm(request)
+
+    context= {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return redirect('main:login')
